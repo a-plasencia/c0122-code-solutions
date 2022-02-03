bae40edfb13c94528b2f964cc6ae0e69e79a913e
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-02 20:01:17 -0800

    wip javascript-loops.

---
+++ javascript-loops/main.js
@@ -0,0 +1,17 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while (currentNumber <= 10) {
+    numbers.push(currentNumber);
+    currentNumber += 1;
+  }
+  return numbers;
+}
