4da93d1469dd204027a33108c74a74f1f5046653
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-09 22:31:22 -0800

    Fixed having a return value in the setValue function and also added pseudo code to the best of my ability.

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,4 +1,10 @@
 /* exported getValue */
+/*
+Create a function that gets the value of what the object sets
+input: (object, 'keyName')
+output: 'value inside keyname'
+-gives back the value inside of the object.
+*/
 
 function getValue(object, key) {
   return object[key];
--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -3,6 +3,12 @@
 Make a function that takes the KeyValuePair and returns an object
 input keyValuePair [array] = [firstName, Alex];
 output object = {firstName : Alex};
+-Create a storage for the object
+-Assign a value to the second value in the array
+-Call the storage, and put the first piece of the list in the
+property, and assign the storage assigned to the second value
+in the array
+return the storage
 */
 function toObject(keyValuePair) {
   var object = {};
