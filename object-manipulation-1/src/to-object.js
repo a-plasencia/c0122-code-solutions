a691fa19155ce818ba9da080164cdf59de669666
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-09 22:07:02 -0800

    Completed object-manipulation-1 exercise.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,4 +1,12 @@
 /* exported toObject */
+/*
+Make a function that takes the KeyValuePair and returns an object
+input keyValuePair [array] = [firstName, Alex];
+output object = {firstName : Alex};
+*/
 function toObject(keyValuePair) {
-
+  var object = {};
+  var value = keyValuePair[1];
+  object[keyValuePair[0]] = value;
+  return object;
 }
