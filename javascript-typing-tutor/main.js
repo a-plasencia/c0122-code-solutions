9d0ee55868b608cdd1814fc0d37540aec137f281
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-12 23:59:53 -0800

    HTML and CSS for javascript-typing-tutor is finished.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,4 @@
+// var $span = document.querySelectorAll('span');
+
+// The following console.log call's return the proper value
+// console.log('the nodelist of $span is: ', $span); <node list of spans
