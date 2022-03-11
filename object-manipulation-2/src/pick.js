607a31ebb607e4ed39f07c9915e7433f5091b50f
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-11 18:10:00 +0000

    WIP object-manipulation-2 almost done with solving algorithm.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,18 @@
 /* exported pick */
+function pick(source, keys) {
+  var pickSource = {};
+  for (var key in source) {
+    // console.log('value of key is: ', key);
+
+    for (var i = 0; i < keys.length; i++) {
+
+      if (key === keys[i]) {
+        var value = source[key];
+        // console.log('value of value is: ', value);
+        pickSource[key] = value;
+        // console.log('value of ' + key + ' and value of keys[i] is: ' + keys[i]);
+      }
+    }
+  }
+  return pickSource;
+}
