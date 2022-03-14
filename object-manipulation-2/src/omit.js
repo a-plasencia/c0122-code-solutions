93c76def60a9ff13be5f4f168e44c42f6c797d97
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-14 05:51:22 +0000

    All object functions are correct but I cannot explain omit so its not finished yet.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,36 +1,20 @@
 /* exported omit */
 
 function omit(source, keys) {
-  // var omitObject = {};
-  // for (var key in source) {
-  //   console.log('value of key is: ', key);
-  //   console.log('value of source[key] is: ', source[key]);
-  //   for (var i = 0; i < keys.length; i++) {
-  //     console.log('value of keys[i] is: ', keys[i]);
-  //     if ([key] !== keys[i]) {
-  //       console.log('YOOOOO this aint in the keys array: ', key);
-  //     }
-  //   }
-  // }
+  var omitObject = {};
+  for (var key in source) {
+    var counter = 0;
+    var firstValue = key;
+    var secondValue = source[key];
+    for (var i = 0; i < keys.length; i++) {
+      if (key !== keys[i]) {
+        counter++;
+      }
+    }
+    if (counter === 2) {
+      omitObject[firstValue] = secondValue;
+    }
+    counter = 0;
+  }
+  return omitObject;
 }
-
-// function pick(source, keys) {
-//   var pickSource = {};
-//   var pickBool = false;
-//   for (var key in source) {
-//     var firstValue = key;
-//     var secondValue = source[key];
-//     for (var i = 0; i < keys.length; i++) {
-//       if (key === keys[i]) {
-//         pickBool = true;
-
-//       }
-//     }
-//     if (pickBool === true && secondValue !== undefined) {
-//       pickSource[firstValue] = secondValue;
-//       pickBool = false;
-//     }
-//   }
-
-//   return pickSource;
-// }
