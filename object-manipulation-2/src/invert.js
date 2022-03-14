a6f76efff65614002f0c12a8e660f25c45e9de9d
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-14 02:13:03 +0000

    Completed invert, will continue through the other functions.

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,12 @@
 /* exported invert */
+
+function invert(source) {
+  var invertedObject = {};
+  for (var key in source) {
+    var firstValue = key;
+    var secondValue = source[key];
+
+    invertedObject[secondValue] = firstValue;
+  }
+  return invertedObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,16 +1,22 @@
 /* exported pick */
+/*
+Have a function where you take an object, see if the key is listed in the array of keys.
+If the key is listed in the array.  Put it in the new object.
+input (object, array) {foo: 1, bar: 2 } [foo, bar]
+output (object) {foo: 1}
+*/
+
 function pick(source, keys) {
   var pickSource = {};
   for (var key in source) {
-    // console.log('value of key is: ', key);
-
+    var firstValue = key;
+    var secondValue = source[key];
     for (var i = 0; i < keys.length; i++) {
-
       if (key === keys[i]) {
-        var value = source[key];
-        // console.log('value of value is: ', value);
-        pickSource[key] = value;
-        // console.log('value of ' + key + ' and value of keys[i] is: ' + keys[i]);
+        // console.log('the value of First value is: ', firstValue);
+        // console.log('the value of the secondValue is: ', secondValue);
+        // console.log('the value of keys[i] is: ', keys[i]);
+        pickSource[firstValue] = secondValue;
       }
     }
   }
