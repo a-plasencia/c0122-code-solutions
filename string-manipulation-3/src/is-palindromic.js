e1dad6549f6813719a474e77f5905950f92d3d2a
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-17 17:07:02 +0000

    WIP on is-palindromic function.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,23 @@
+/* exported isPalindromic */
+/*
+Create a function that checks if the string is a palindrome
+input(string) 'racecar'
+output(boolean) true
+-Create a first string that copies the original string
+-Create a second string so you can compare it with the first string later.
+-Use the split method to check for any spaces in the string.
+-If the length is one,
+  Run for loop to go through the string and start from the length and decrement until zero
+  Push each on to the second string
+*/
+
+// function isPalindromic(string) {
+//   var firstString = string;
+//   var secondString = '';
+//   firstString.join('');
+//   if (firstString === 1) {
+//     for (i = string.length; i > 0; i--) {
+
+//     }
+//   }
+// }
