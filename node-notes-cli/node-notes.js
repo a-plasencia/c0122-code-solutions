ded19d73173c86cd02efbb266c6c8519d1e07c54
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-17 22:57:40 +0000

    The create functionality now works.

--- node-notes-cli/node-notes.js
+++ node-notes-cli/node-notes.js
@@ -1,8 +1,28 @@
-const data = require('./data.json');
+let data = require('./data.json');
+const fs = require('fs');
+const create = require('./data-add.js');
 const command = process.argv[2];
-console.log(command);
-console.log(data);
-console.log(data.nextId);
-for (var key in data.notes) {
-  console.log(`${key}: ${data.notes[key]}`);
+const createMessage = process.argv[3];
+
+if (command === 'read') {
+  for (var key in data.notes) {
+    console.log(`${key}: ${data.notes[key]}`);
+
+  }
+  data = JSON.stringify(data, null, 2);
 }
+
+if (command === 'create') {
+  create(createMessage);
+
+  data.nextId++;
+  data = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', data, err => {
+    if (err) throw err;
+  });
+}
+
+// successful test console.logs stay here
+// console.log(command);
+// console.log(data);
+// console.log(data.nextId);
