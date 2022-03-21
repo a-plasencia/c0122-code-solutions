8214d0bca93f2e00bcf6e3035893b2441d12dec6
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-21 18:03:34 +0000

    WIP express-post-json.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-const grades = {};
-// const nextId = 1;
 
-const gradesArray = Object.values(grades);
+const app = express();
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+const grades = {};
+let nextId = 1;
 
 app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+
+  for (var key in grades) {
+    gradesArray.push(grades[key]);
+  }
   res.json(gradesArray);
 });
 
-// app.post('/api/grades', (res, req) => {
-//   res.statusCode(201);
-//   console.log('Hi your data is being processed');
-//   res.send(req.body);
-// });
+app.post('/api/grades', (res, req) => {
+  const newGrade = req.body;
+  const id = nextId;
+  newGrade.id = id;
+  nextId++;
+  grades[id] = newGrade;
+  res.statusCode(201).json(newGrade);
+
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
