6a5c33d3b57a5cf65d4c5ec8c06f19f8d9a68eef
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-22 01:04:45 +0000

    WIP post-api-notes

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const fs = require('fs');
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
+const error = {
+  get404: {
+    error: 'cannot find note with id '
+  },
+  get400: {
+    error: 'id must be a positive integer'
+  },
+  post400: {
+    error: 'content is a required field'
+  },
+  post500: {
+    error: 'An unexpected error occurd'
+  }
+};
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -12,7 +30,6 @@ app.get('/api/notes', (req, res) => {
   for (const key in data.notes) {
     notesArray.push(data.notes[key]);
   }
-
   res.status(200).json(notesArray);
 });
 
@@ -20,23 +37,27 @@ app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   if (id > 0) {
     if (!data.notes[id]) {
-      res.status(404).send(
-        `
-        {
-          "error": "cannot find note with id ${id}"
-        }
-        `);
+
+      res.status(404).json(error.get404);
     } else {
       res.status(200).json(data.notes[id]);
     }
   } else if (id < 0) {
-    console.error('The number you entered was negative, please enter a positve integer');
-    res.status(400).send(
-      `
-      {
-        "error": "id must be a positive integer"
-      }
-      `
-    );
+    res.status(400).json(error.get400);
   }
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const id = data.nextId;
+  newNote.id = id;
+  data.notes[id] = newNote;
+  data.nextId++;
+  const dataString = JSON.stringify(data, null, 2);
+  fs.writeFile('derp/data.json', dataString, err => {
+    if (err) {
+      res.status(500).json(error.post500);
+      console.error(err);
+    } else { res.status(201).json(newNote); }
+  });
 });
