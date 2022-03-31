d740d15b7f47805d50046a391a85d0f93c291e1e
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-31 20:27:57 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+class Pokemon extends React.Component {
+  render() {
+    return (
+      <ui>
+        {pokedex.map(pokedex => (
+          <li key={pokedex.id}>
+            {pokedex.name}
+          </li>
+        ))}
+      </ui>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Pokemon />,
+  document.querySelector('#root')
+);
