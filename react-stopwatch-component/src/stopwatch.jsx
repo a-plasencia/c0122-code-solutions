962f1acd443880507dc4ac1c457e64ffb067a3f8
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-03 01:07:44 +0000

    Completed react-stopwatch-component challenge.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -7,17 +7,35 @@ class Stopwatch extends React.Component {
       isToggleOn: false,
       isTimer: 0
     };
-
     this.handleClick = this.handleClick.bind(this);
     this.handleTimer = this.handleTimer.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   handleTimer() {
+    if (this.state.isToggleOn === false) {
+      this.timerId = setInterval(() => this.tick(), 1000
+      );
+    } else {
+      clearInterval(this.timerId);
+    }
+  }
 
+  reset() {
+    if (this.state.isToggleOn === false) {
+      this.setState({
+        isTimer: 0
+      });
+    }
+  }
+
+  tick() {
+    this.setState({
+      isTimer: this.state.isTimer + 1
+    });
   }
 
   handleClick() {
-
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
@@ -34,11 +52,10 @@ class Stopwatch extends React.Component {
   }
 
   render() {
-
     const boolean = this.isToggle();
     return (
     <React.Fragment>
-      <button>{this.state.isTimer}</button>
+      <button onClick={this.reset}>{this.state.isTimer}</button>
       <i onClick={this.handleClick} className={`fa-solid ${boolean}`}></i>
     </React.Fragment>
     );
