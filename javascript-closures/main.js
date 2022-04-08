3527c13c3708c66ef7ec1200c1629ee8ddb0c19e
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-08 19:52:49 +0000

    WIP javascript-closures exercise.

---
+++ javascript-closures/main.js
@@ -0,0 +1,13 @@
+// function graduate(credential) {
+//   function fullName(string) {
+
+//   }
+//   return `${string} ${credential}`;
+// }
+
+// const medicalSchool = graduate('M.D.');
+// const lawSchool = graduate('Esq.');
+
+// console.log(medicalSchool.fullName('Dan Abramov'));
+
+// console.log(lawSchool.fullName('Ryan Florence'));
