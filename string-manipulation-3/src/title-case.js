1b38899901fb3f06df05642e9cbef2b893a2af06
Alex Plasencia <aPlasencia895@hotmail.com>
2022-05-04 19:59:52 +0000

    Completed string-manipulation-3 algorithms.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,7 +1,23 @@
 /* exported titleCase */
 
 /*
-
+Create a function that takes a string and returns an APA Title Case Style
+input title (string) 'composing software'
+output (string) 'Composing Software'
+- Create a variable string that saves the value of the variable title
+- Call the toLowerCase method on the string
+- Call the split method and split the string into an array wherever there is a space (' ')
+- Loop through the list of words in the string array
+- If the word is 'javascript' or 'javascript:' capitalize the J and S in JavaScript
+- If the word is 'api' capitalize it to 'API'
+- If it is the first word in the array capitalize the word
+- If the word is greater than or equal to five letters capitzlie the array
+- If the word is not one of the "minor words of three letters or fewer" capitalize the word
+- Loop through each letter of the word
+- If the word has a hyphen (-), capitalize the next letter in the word
+- If the word has a colon (:), capitalize the next word in the word array
+- Call the join method on the string to turn the array into a string
+- Return the string which is now capitalized in APA
 */
 
 function titleCase(title) {
@@ -41,8 +57,8 @@ function titleCase(title) {
         string[i + 1] = string[i + 1].charAt(0).toUpperCase() + string[i + 1].substring(1);
       }
     }
-
   }
+
   string = string.join(' ');
   return string;
 }
