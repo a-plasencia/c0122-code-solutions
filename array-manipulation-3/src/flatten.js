d78f92b736f7a0c970ea4dcf4995720e5173b25f
Alex Plasencia <aPlasencia895@hotmail.com>
2022-05-05 00:20:43 +0000

    Completed and pseudocoded flatten.

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,28 @@
 /* exported flatten */
+
+/*
+Write a function that "flattens" an array by one level
+input (array) [["foo", "bar"], ["baz", "qux"]]
+output (array) ["foo", "bar", "baz", "qux"]
+- Assign a new empty array
+- Loop through the given array
+- Check if there is an array in the array you are looping through
+- If it's true loop through the array of an array that is found
+- Push the contents of the array of an array into the created array
+- If there is no array in the array, push it in the created array
+- Return the created array
+*/
+
+function flatten(array) {
+  const flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      for (let j = 0; j < array[i].length; j++) {
+        flattenedArray.push(array[i][j]);
+      }
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  return flattenedArray;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,16 @@
 /* exported zip */
+
+function zip(first, second) {
+
+  const zippedArray = [];
+  const minizip = [];
+  for (let i = 0; i < first.length; i++) {
+    minizip.push(first[i]);
+    minizip.push(second[i]);
+    zippedArray.push(minizip);
+  }
+  // console.log(zippedArray);
+  // console.log(minizip);
+
+  return zippedArray;
+}
