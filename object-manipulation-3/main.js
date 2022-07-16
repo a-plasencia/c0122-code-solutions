86b0534570c1f6b75f78ae65480c2f5b8d53ec0f
Alex Plasencia <aPlasencia895@hotmail.com>
2022-07-16 04:14:05 +0000

    Completed step 1 of object-manipulation-3.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,19 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+const p1 = {
+  name: 'Adrian',
+  hand: []
+};
+const p2 = {
+  name: 'Anthony',
+  hand: []
+};
+const p3 = {
+  name: 'Dina',
+  hand: []
+};
+const p4 = {
+  name: 'Matt',
+  hand: []
+};
+console.log('Checking the contents of each player', p1, p2, p3, p4);
